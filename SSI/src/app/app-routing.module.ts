9a34dac3c6b7f00a44f252e7059f66bab56f7bde
Delside <43532163+Delside@users.noreply.github.com>
2020-12-29 23:04:29 +0100

    Created website structure, added npm MongoDB

--- SSI/src/app/app-routing.module.ts
+++ SSI/src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { Routes, RouterModule } from '@angular/router';
+import { FormComponent } from './components/form/form.component';
+import { MainSiteComponent } from './components/main-site/main-site.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'main-site',
+    component: MainSiteComponent
+  },
+  {
+    path: 'form',
+    component: FormComponent
+  },
+  {
+    path: '*',
+    redirectTo: 'main-site'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- SSI/src/app/app.module.ts
+++ SSI/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { MainSiteComponent } from './components/main-site/main-site.component';
+import { FormComponent } from './components/form/form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    MainSiteComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule,
